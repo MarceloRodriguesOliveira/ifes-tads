9fac66a9d230e1bfe7ea02deabe07ebf890a403a
MarceloRodriguesOliveira <marcelo--rodrigues2@hotmail.com>
2025-11-22 16:03:19 -0300

    Removendo pasta C-Programming-Language-Ex do versionamento

--- C-Programming-Language-Ex/ch1/ex1-06.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Expression value: %d\n", getchar() != EOF);
-}
--- C-Programming-Language-Ex/ch1/ex1-07.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int c = getchar();
-    printf("EOF: %d\n", EOF);
-    return 0;
-}
